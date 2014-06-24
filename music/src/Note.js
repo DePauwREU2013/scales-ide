326cafbbb7ab2f799b22aecbfaaeb69029f8b015
namchid <ndo@trinity.edu>
2014-06-24 17:30:48 -0400

    modified - wip

--- music/src/Note.js
+++ music/src/Note.js
@@ -1,32 +1,50 @@
-/* Purpose of Note.js: to abstract away some of the elements of
+/* 
+** Purpose of Note.js: to abstract away some of the elements of
 ** Web Audio API, such as setting up an audio context and
 ** connecting nodes.
 */
 
 
-// @params: a library note (todo) or path to any sound
-// initializes by setting up audio context and loads sound into memory
-function Note(note) {
-	var context = new AudioContext();
+/* 
+** @params: a library note (todo) or path to any sound
+** initializes a new instance of Note by setting up 
+** a context (if none exists), and loading the note
+*/
+function Note(note, volume) {
+	if(!context) {
+		context = new AudioContext();
+	}
+
 	var sound = this;
+	setProperties(sound, note, volume);
 
-	var soundReq = new XMLHttpRequest();
-	soundReq.open("GET", note, true);
-	soundReq.responseType = "arraybuffer";
+	var req = new XMLHttpRequest();
+	req.open("GET", sound.note, true);
+	req.responseType = "arraybuffer";
 
-	soundReq.onload = function(){
-		context.decodeAudioData(soundReq.response, function(buffer)){
+	req.onload = function() {
+		context.decodeAudioData(req.response, function(buffer) {
 			sound.buffer = buffer;
+			sound.loaded = true;
 		});
 	}
-	soundReq.send();
 }
 
-// sets attributes for note, such as oscillation, pan, etc.
-// possibly break up into smaller functions
-Note.prototype.setAttributes() {
+/*
+** @params: the note and volume for NoteObj
+** sets NoteObj's properties
+*/
+function setProperties(NoteObj, note, volume) {
+	NoteObj.note = note;
+	NoteObj.panner = context.createPanner();
+	NoteObj.volume = context.createGain();
 
+	NoteObj.note = note;
+	NoteObj.buffer = null;
+	NoteObj.loaded = false;
+	NoteObj.volume.gain.value = (!volume) ? 1 : volume;
 }
+
 
 // plays the note immediately, or after delay@params
 Note.prototype.play() {
