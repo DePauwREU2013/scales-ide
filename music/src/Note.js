045dd235c6a68f993267f063f44c4c2577994154
namchid <ndo@trinity.edu>
2014-06-23 21:18:56 -0400

    outline for possible methods

--- music/src/Note.js
+++ music/src/Note.js
@@ -2,3 +2,30 @@
 ** Web Audio API, such as setting up an audio context and
 ** connecting nodes.
 */
+
+
+// initializes a note (any sound), sets up audio context, loads sound
+function Note() {
+
+}
+
+// sets attributes for note, such as oscillation, pan, etc.
+// possibly break up into smaller functions
+Note.prototype.setAttributes() {
+
+}
+
+// plays the note immediately, or after delay@params
+Note.prototype.play() {
+
+}
+
+// immediately stops sound
+Note.prototype.stop() {
+
+}
+
+// sets position of AudioListener and panner node
+Note.prototype.setListenerEnvironment() {
+
+}
