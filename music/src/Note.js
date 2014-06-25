1b2a965fc6317e1eed8166c41c4728ab230164c7
namchid <ndo@trinity.edu>
2014-06-24 23:31:34 -0400

    updated

--- music/src/Note.js
+++ music/src/Note.js
@@ -10,39 +10,39 @@
 ** initializes a new instance of Note by setting up 
 ** a context (if none exists), and loading the note
 */
-function Note(note, volume) {
-	if(!context) {
+function Note(source, level) {
+	if(!window.context) {
 		context = new AudioContext();
 	}
 
-	var sound = this;
-	setProperties(sound, note, volume);
+	var note = this;
+	settings(note, source, level);
 
 	var req = new XMLHttpRequest();
-	req.open("GET", sound.note, true);
+	req.open("GET", note.source, true);
 	req.responseType = "arraybuffer";
 
 	req.onload = function() {
 		context.decodeAudioData(req.response, function(buffer) {
-			sound.buffer = buffer;
-			sound.loaded = true;
+			note.buffer = buffer;
+			note.loaded = true;
 		});
 	}
+	req.send();
 }
 
 /*
 ** @params: the note and volume for NoteObj
 ** sets NoteObj's properties
 */
-function setProperties(NoteObj, note, volume) {
-	NoteObj.note = note;
-	NoteObj.panner = context.createPanner();
-	NoteObj.volume = context.createGain();
+function settings(note, source, level) {
+	note.source = source;
+	note.panner = context.createPanner();
+	note.volume = context.createGain();
 
-	NoteObj.note = note;
-	NoteObj.buffer = null;
-	NoteObj.loaded = false;
-	NoteObj.volume.gain.value = (!volume) ? 1 : volume;
+	note.buffer = null;
+	note.loaded = false;
+	note.volume.gain.value = (!level) ? 1 : level;
 }
 
 
@@ -50,27 +50,27 @@ function setProperties(NoteObj, note, volume) {
 ** @params: plays note after delay,
 ** or immediately if no argument
 */
-Note.prototype.start(delay) {
-	if(this.loaded) {
-		var soundSource = context.createBufferSouce();
+Note.prototype.play = function(delay) {
+	if(this.loaded === true) {
+		var soundSource = context.createBufferSource();
 		soundSource.buffer = this.buffer;
 		soundSource.connect(this.panner);
 		this.panner.connect(this.volume);
 		this.volume.connect(context.destination);
-		
-		// older systems may not support start()
-		try {
+         
+        // older systems (i.e IE) may not support start()
+        try {
+        	if(!delay) 
+        		soundSource.start(0);
+        	else 
+        		soundSource.start(delay);
+        }
+        catch(err) {
 			if(!delay) 
-				soundSource.start(0);
-			else
-				soundSource.start(delay);
-		}
-		catch(err) {
-			if(!delay)
 				soundSource.noteOn(0);
-			else
+			else 
 				soundSource.noteOn(delay);
-		}
+		} 
 	}
 }
 
@@ -78,9 +78,9 @@ Note.prototype.start(delay) {
 ** @params: sets gain for Note
 ** @return: a new instance of the Note with set volume
 */
-Note.prototype.setVolume(vol) {
+Note.prototype.setVolume = function(level) {
 	var newNote = this;
-	newNote.volume.gain.value = vol;
+	newNote.volume.gain.value = level;
 	return newNote;
 }
 
