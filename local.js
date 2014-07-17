642a3583ba71aace7b8934712058a2b6fc4e8ef4
Benjamin J. Kruger <bekroogle@gmail.com>
2014-07-17 18:03:08 -0400

    Factored out jQuery-UI initialization.

--- local.js
+++ local.js
@@ -20,75 +20,11 @@ $(document).ready(function() {
 	
 	init_canvas();	
 	
+	init_jquery_ui();
 
 	// document.getElementById('openLocalFile').addEventListener('change', readFile, false);
 	
-	// jQuery UI (resizable, droppable, etc.)
-	$('#header').resizable({
-		handles: "s",
-	});
-
-	$('#header').resize( function() {
-		$('#panels').css('top', parseInt($(this).css('height')) + 9 + "px");
-		$('#autodiv').css("left", $("#resizable").css("width"));
-	    $('#autodiv').css("right", "0");
-		$('#context-list').css('top', parseInt($(this).css('height')) + 9 + "px");
-		$('canvas').attr('width', $('#autodiv').css('width'));
-		$('canvas').attr('height', $('#autodiv').css('height'));
-		render();
-	});
 	
-	// Make context-list resizable:
-	  $('#context-list').resizable( {
-	    handles: "e"
-	  });
-	
-	// Automatically resize the panels to the right:
-	  $('#context-list').on("resize", function() {
-	    $('#panels').css('left', parseInt($('#context-list').css('width'))-10 + 'px');
-	    $('canvas').attr('width', $('#autodiv').css('width'));
-		$('canvas').attr('height', $('#autodiv').css('height'));
-	  	render();
-	  });
-	
-	// Set resizable container for left Ace editor:
-	  $( "#resizable" ).resizable( {
-	    // Handle can be selected as $('.ui-resizable-e').
-	    handles: "e"
-	  });
-	
-	// Triggered when panel is resized:
-	  $("#resizable").resize( function() {
-	
-	// Notify Ace to update its size:
-	    editor.resize();
-	
-	// Automatically resize right panel to fill the
-	// remainder of div#panels:
-	    $('#autodiv').css("left", $("#resizable").css("width"));
-	    $('#autodiv').css("right", "0");
-		$('#current-file').css('right',$('#resizable').css('right'));
-		$('canvas').attr('width', $('#autodiv').css('width'));
-		$('canvas').attr('height', $('#autodiv').css('height'));
-		render();
-    	
-	  }); 
-	
-	
-	// Make the panels received drag & dropped items:
-	  $('.droppable').droppable( {
-	    accept: '.draggable',
-	    over: function() {
-	      $('.editor').addClass('highlight');
-	    },
-	    out: function() {
-	      $('.editor').removeClass('highlight');
-	    },
-	    drop: function() {
-	      load_file($(this).attr('id'));
-	      console.log(this);
-	    }
-	  })
 
 	  // Syntax checking/error reporting
 	  editor.on("change", function(e) {
@@ -127,17 +63,25 @@ $(document).ready(function() {
 
 }); // $(document).ready
 
+/** load_file_tree
+ * Initializes fancy tree element using the urrent workspace object
+ * as its JSON source.
+ */
 function load_file_tree() {
+
+	// Create the fancytree object.
 	$('#tree').fancytree({  		
 		source: workspace_object,
-		// [{"title":"Project1","key":"1","folder":true,"children":[{"title":"main.scala","key":"2","contents":"this is the contents of the file","language":"scala"},{"title":"libscales.scala","key":"3","contents":"itscales!","language":"scala"}]},{"title":"Project2","key":"4","folder":true,"children":[{"title":"main.scala","key":"5","contents":"thisisthecontentsofthefile","language":"scala"},{"title":"libscales.scala","key":"6","contents":"itscales!","language":"scala"}]}],
+
+		// When a node is activated (clicked/keyboard):
 		activate: function(event, data){
-			// A node was activated: display its title:
-			
-			var node = data.node;
-		
-			editor.setValue(node.data.contents);
-			console.dir(node);
+			var node = data.node;		
+
+			// If the node is a file, load its contents to editor:
+			if (!node.folder) {
+				editor.setValue(node.data.contents);
+			}
+
 		}, 
 		extensions: ["themeroller"]
 	  });
@@ -217,6 +161,75 @@ function init_canvas() {
 	render();
 }
 
+function init_jquery_ui() {
+// jQuery UI (resizable, droppable, etc.)
+	$('#header').resizable({
+		handles: "s",
+	});
+
+	$('#header').resize( function() {
+		$('#panels').css('top', parseInt($(this).css('height')) + 9 + "px");
+		$('#autodiv').css("left", $("#resizable").css("width"));
+	    $('#autodiv').css("right", "0");
+		$('#context-list').css('top', parseInt($(this).css('height')) + 9 + "px");
+		$('canvas').attr('width', $('#autodiv').css('width'));
+		$('canvas').attr('height', $('#autodiv').css('height'));
+		render();
+	});
+	
+	// Make context-list resizable:
+	  $('#context-list').resizable( {
+	    handles: "e"
+	  });
+	
+	// Automatically resize the panels to the right:
+	  $('#context-list').on("resize", function() {
+	    $('#panels').css('left', parseInt($('#context-list').css('width'))-10 + 'px');
+	    $('canvas').attr('width', $('#autodiv').css('width'));
+		$('canvas').attr('height', $('#autodiv').css('height'));
+	  	render();
+	  });
+	
+	// Set resizable container for left Ace editor:
+	  $( "#resizable" ).resizable( {
+	    // Handle can be selected as $('.ui-resizable-e').
+	    handles: "e"
+	  });
+	
+	// Triggered when panel is resized:
+	  $("#resizable").resize( function() {
+	
+	// Notify Ace to update its size:
+	    editor.resize();
+	
+	// Automatically resize right panel to fill the
+	// remainder of div#panels:
+	    $('#autodiv').css("left", $("#resizable").css("width"));
+	    $('#autodiv').css("right", "0");
+		$('#current-file').css('right',$('#resizable').css('right'));
+		$('canvas').attr('width', $('#autodiv').css('width'));
+		$('canvas').attr('height', $('#autodiv').css('height'));
+		render();
+    	
+	  }); 
+	
+	
+	// Make the panels received drag & dropped items:
+	  $('.droppable').droppable( {
+	    accept: '.draggable',
+	    over: function() {
+	      $('.editor').addClass('highlight');
+	    },
+	    out: function() {
+	      $('.editor').removeClass('highlight');
+	    },
+	    drop: function() {
+	      load_file($(this).attr('id'));
+	      console.log(this);
+	    }
+	  })	
+}
+
 /** render()
  * renders the red square on the canvas.
  */
@@ -226,3 +239,4 @@ function render() {
 	ctx.fillStyle = "#FF0000";
 	ctx.fillRect(0,0,50,50);
 }
+
