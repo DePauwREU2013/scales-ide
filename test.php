9c851e9a736d9c2d43eb4305a101ff693ebaf928
Benjamin J. Kruger <bekroogle@gmail.com>
2014-07-24 16:14:56 -0400

    test.php responds with string of Javascript.

--- test.php
+++ test.php
@@ -2,10 +2,8 @@
 	/* This is just a simple script that responds to a post with 
 	 * a short message and then echos the text that was posted. */
 
-	$postdata = file_get_contents("php://input");
-	echo "Hello! I'm an imaginary scala-js compile server...\r\n";
-	echo "This is what you posted to me. I would be extracting and \r\n";
-	echo "compiling the source code I'd be trying to compile if I was \r\n";
-	echo "real:\r\n\r\n";
-	echo $postdata;
+	echo "var c = document.querySelector('canvas');"
+		."var ctx = c.getContext('2d');"
+		."ctx.fillStyle = '#FF0000';"
+		."ctx.fillRect(0,0,50,50);"
 ?>
