ca1170609005df0ac0327dbe4f7150f95276b090
Benjamin J. Kruger <bekroogle@gmail.com>
2014-07-24 16:21:32 -0400

    Php writes file, responds with filename.

--- test.php
+++ test.php
@@ -2,8 +2,14 @@
 	/* This is just a simple script that responds to a post with 
 	 * a short message and then echos the text that was posted. */
 
-	echo "var c = document.querySelector('canvas');"
-		."var ctx = c.getContext('2d');"
-		."ctx.fillStyle = '#FF0000';"
-		."ctx.fillRect(0,0,50,50);"
+	$file = 'worker.js';
+	$fp = fopen($file, "w");
+	$output = 	 "var c = document.querySelector('canvas');".PHP_EOL
+				."var ctx = c.getContext('2d');".PHP_EOL
+				."ctx.fillStyle = '#FF0000';".PHP_EOL
+				."ctx.fillRect(0,0,50,50);";
+	fwrite($fp, $output);
+	fclose($fp);
+
+	echo "worker.js";
 ?>
